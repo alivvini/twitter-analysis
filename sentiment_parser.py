3e5ac5e492450a341f1661d1726999b42023fe39
Hivini <vinicioquinterobuss@gmail.com>
2019-03-19 10:38:46 -0600

    Did a better functions

--- sentiment_parser.py
+++ sentiment_parser.py
@@ -12,11 +12,18 @@ def analyze_sentiment(tweet):
     analyzer = SentimentIntensityAnalyzer()
     for sentence in tweet['replies']:
         vs = analyzer.polarity_scores(sentence['text'])
-        print("{:-<65} {}".format(sentence['text'], str(vs)))
+        compound = vs['compound']
+        sentiment = 0
+        if compound >= 0.05:
+            # Positive
+            sentiment = 1
+        elif compound <= -0.05:
+            # Negative
+            sentiment = -1
+        print(sentiment)
+        # print("{:-<65} {}".format(sentence['text'], str(sentiment)))
         analyze_sentiment(sentence)
 
 
 tweet_json = load_json(1105974640592281601)
 analyze_sentiment(tweet_json)
-
-
